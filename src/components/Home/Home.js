9a82028fe5e3337d358bc0c929d7746a6ea0ae1d
Chase Derr <Chisnits@users.noreply.github.com>
2017-08-02 14:39:55 -0600

    added Login component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Gallery from "./Gallery"
+import Login from "./Login"
+import "./Styles/Home.css"
+
 
 class Home extends Component {
   render() {
     return (
-      <div>
-        <Gallery />
+      <div className="home-wrapper">
+          <Gallery />
+          <Login />
       </div>
     );
   }
 }
+
+
+
 
 export default Home;
