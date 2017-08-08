1045c3feb839b30d29ca4ff4fdc07648d793dc08
Chase Derr <Chisnits@users.noreply.github.com>
2017-08-08 15:31:41 -0600

    added basic calender

--- src/components/Calender/Calender.js
+++ src/components/Calender/Calender.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
+import InfiniteCalendar from 'react-infinite-calendar';
+import 'react-infinite-calendar/styles.css'; // Make sure to import the default stylesheet
 
 class Calender extends Component {
+       
     render() {
+        // Render the Calendar
+            var today = new Date();
+            var lastWeek = new Date(today.getFullYear(), today.getMonth(), today.getDate() - 7);
         return (
             <div>
-                Calender
+                <InfiniteCalendar
+                    width={400}
+                    height={600}
+                    selected={today}
+                    disabledDays={[0,6]}
+                    minDate={lastWeek}
+                />
             </div>
         );
     }
