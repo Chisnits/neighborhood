77680890b0d0a762776513535b7dd22541d427b5
Chase Derr <Chisnits@users.noreply.github.com>
2017-08-09 15:04:31 -0600

    directory

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 
 class Directory extends Component {
+    handleCoolness() {
+        const chase = "the coolest"
+    }
     render() {
         return (
             <div>
