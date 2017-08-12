34b562256b08b3541747026a4431638fcb8ea7e1
Chase Derr <Chisnits@users.noreply.github.com>
2017-08-11 21:30:30 -0700

    directory

--- src/components/Directory/Directory.js
+++ src/components/Directory/Directory.js
@@ -1,9 +1,7 @@
 import React, { Component } from 'react';
 
 class Directory extends Component {
-    handleCoolness(person){
-        alert(`${person} is cool`);
-    }
+    
     render() {
         return (
             <div>
