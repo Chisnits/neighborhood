b9ada9ad1ad3d3e5a6a165c527e581462d569fcd
Chase Derr <Chisnits@users.noreply.github.com>
2018-02-25 12:32:02 -0700

    changed link animation to transition.

--- src/components/Header.js
+++ src/components/Header.js
@@ -108,7 +108,6 @@ class Header extends Component {
                 </div>
                 <div className="login-wrapper">
                     <div>
-                        <div></div>
                     {isLoggedIn ? 
                         <form>
                             <Link to="/"><input className="btn" type="submit" value="Logout" onClick={this.handleLogout} /> </Link>
